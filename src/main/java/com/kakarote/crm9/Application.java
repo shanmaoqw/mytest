d2a999e0dc52eb01026eeac079a0b9a138f6d41a
wuguang <837571206@qq.com>
2019-09-03 11:58:11 +0800

    mod

--- src/main/java/com/kakarote/crm9/Application.java
+++ src/main/java/com/kakarote/crm9/Application.java
@@ -6,9 +6,6 @@ import com.kakarote.crm9.common.constant.BaseConstant;
 
 public class Application {
     public static void main(String[] args) {
-        UndertowServer
-                .create(JfinalConfig.class,"config/undertow.txt")
-                .setResourcePath("src/main/webapp,"+ BaseConstant.UPLOAD_PATH)
-                .start();
+        xxxxxxxxxxxxxxxxxx
     }
 }
